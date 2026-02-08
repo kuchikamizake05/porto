19f0638e13c875dc589af6c344d42c6c65b41f3f
kuchikamizake05 <faaidsakhaa2005@mail.ugm.ac.id>
2026-02-08 22:45:20 +0700

    fix: update import statement for PrismaClient to use prismaModule

--- be/src/db.ts
+++ be/src/db.ts
@@ -1,6 +1,8 @@
-import { PrismaClient } from "./generated/prisma/index.js";
+import prismaModule from "./generated/prisma/index.js";
 import { PrismaLibSql } from "@prisma/adapter-libsql";
 import "dotenv/config";
+
+const PrismaClient = prismaModule.PrismaClient;
 
 const adapter = new PrismaLibSql({
   url: process.env.DATABASE_URL || "file:./dev.db",
